77dfe083fcf86875ba142e4c43eab8641cc1de52
szdani <sz.daniel.attila@gmail.com>
2019-05-15 21:50:45 +0200

    Classificator with repeated pipeline, reshape(64,64)

--- src/classificator/model.py
+++ src/classificator/model.py
@@ -6,12 +6,18 @@ import tensorflow as tf
 
 model = tf.keras.Sequential()
 model.add(tf.keras.layers.Flatten())
-model.add(tf.keras.layers.Dense(1024*1024))
+model.add(tf.keras.layers.Dense(64*64))
+model.add(tf.keras.layers.Dense(16, activation='relu'))
+model.add(tf.keras.layers.Dense(1, activation='softmax'))
+
+model.compile(optimizer=tf.keras.optimizers.Adam(0.001), loss='categorical_crossentropy', metrics=['accuracy'])
 
 
+tf_records_path = os.path.join('../../data/tf_records/', 'classificator_0/*.tfrecords')
+dataset = classificator_pipeline(tf_records_path,
+                                 batch_size=5,
+                                 io_parallel_calls=2,
+                                 file_parsing_parallelism=1,
+                                 augmentation_parallelism=2)
 
-#tf_records_path = os.path.join(tf_record.TF_RECORDS_PATH, 'classificator_0/*.tfrecords')
-#print(tf_records_path)
-#dataset = classificator_pipeline(tf_records_path)
-
-#model.compile()
+model.fit(dataset, epochs=30, steps_per_epoch=15)
--- src/classificator/pipeline.py
+++ src/classificator/pipeline.py
@@ -9,7 +9,7 @@ def classificator_pipeline(tf_records_path, batch_size=1, io_parallel_calls=1, f
                            augmentation_parallelism=1):
     print("Reading from " + tf_records_path)
 
-    filenames = tf.data.Dataset.list_files(tf_records_path, shuffle=True)
+    filenames = tf.data.Dataset.list_files(tf_records_path, shuffle=True).repeat()
     dataset = tf.data.TFRecordDataset(filenames, compression_type='GZIP', num_parallel_reads=io_parallel_calls)
     dataset = dataset.map(map_func=tf_record.ClassificatorTFRecordHandler.parse_tf_record,
                          num_parallel_calls=file_parsing_parallelism)
@@ -17,6 +17,9 @@ def classificator_pipeline(tf_records_path, batch_size=1, io_parallel_calls=1, f
     # Cast to float
     cast = lambda x, y: (tf.cast(x, tf.dtypes.float32), tf.cast(y, tf.dtypes.float32))
     dataset = dataset.map(cast, num_parallel_calls=augmentation_parallelism)
+
+    reshape = lambda x, y: (tf.reshape(x, (1024, 1024, 1)), y)
+    dataset = dataset.map(reshape, num_parallel_calls=augmentation_parallelism)
 
     # Clip into [0, 300]
     clip = lambda x, y: (tf.clip_by_value(x, 0.0, 300.0), y)
@@ -32,21 +35,31 @@ def classificator_pipeline(tf_records_path, batch_size=1, io_parallel_calls=1, f
     # map_and_batch also available but our batch size is < 100
     dataset = dataset.batch(batch_size)
 
+    # Reshape
+    resize = lambda x, y: (tf.image.resize(x, (32,32)), y)
+    dataset = dataset.map(resize, num_parallel_calls=augmentation_parallelism)
+
     return dataset
 
 
+if __name__ == '__main__':
 
-#tf.enable_eager_execution()
-tf_records_path = os.path.join(tf_record.TF_RECORDS_PATH, 'classificator_0/*.tfrecords')
-dataset = classificator_pipeline(tf_records_path)
+    #tf.enable_eager_execution()
+    tf_records_path = os.path.join(tf_record.TF_RECORDS_PATH, 'classificator_0/*.tfrecords')
+    dataset = classificator_pipeline(tf_records_path)
 
-img = iter(dataset)
-img = next(img)
+    img = iter(dataset)
+    img = next(img)
 
+    for data in iter(dataset):
+        print(img[0].shape, img[1])
+        print(data[1], data[0].shape)
 
-import matplotlib.pyplot as plt
+    import matplotlib.pyplot as plt
 
-print(img[0].shape)
-print(img[1].shape)
-plt.imshow(img[0][0], cmap='Greys_r')
-plt.show()
+    print(img[0].shape)
+    print(img[1].shape)
+    print(img[1])
+    print(img[0][0,:,:,0].shape)
+    plt.imshow(img[0][0,:,:,0], cmap='Greys_r')
+    plt.show()
